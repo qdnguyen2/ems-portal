0685beafd7e7c4f993f38b1c7a35bb57018bcb35
Quan <qdnguyen2@yahoo.com>
2020-02-11 21:47:53 -0800

    Add assigment description

--- src/app/servers/servers/servers.component.ts
+++ src/app/servers/servers/servers.component.ts
@@ -46,6 +46,12 @@ export class ServersComponent implements OnInit {
     this.userName = (<HTMLInputElement>event.target).value;
   }
 
+  /* Assignment for the data binding  
+  1. Add an input field which updates a property ('username') via two-way-binding
+  2. Output the username property vis String interpolation (in a paragraph below the input)
+  3. Add a button which may only be clicked if the username is NOT an empty string
+  4. Upon clicking the button, the username should be reset to an emtpy string
+  */
   onClearUserName(){
     this.userName = '';
   }
